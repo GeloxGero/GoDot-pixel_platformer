f29ee44e3d659547a2dcda1d5c0bd5664ff1481e
GeloxGero <gelo12gero@gmail.com>
2023-09-19 22:58:04 +0800

    finished title screen

---
+++ Scripts/TitleScreen.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://assets/World.tscn")
+	pass # Replace with function body.
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
