cb348d0a1869cf43d5efb4b0f468bcb5c1238554
GeloxGero <gelo12gero@gmail.com>
2023-09-21 17:58:44 +0800

    world block

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,10 +1,11 @@
 extends KinematicBody2D
 
 # Member variables
-var speed = 200
-var jump_speed = -500 # Negative because 2D space's y-axis is down
+var speed = 140
+var jump_speed = -250 # Negative because 2D space's y-axis is down
 var gravity = 980 # The force of gravity
 var velocity = Vector2()
+var jump_force = 1
 
 func _physics_process(delta):
 	# Get input direction
@@ -25,7 +26,15 @@ func _physics_process(delta):
 
 	# Jumping
 	if Input.is_action_just_pressed('ui_up') and is_on_floor():
-		velocity.y = jump_speed
+		velocity.y = jump_speed * jump_force
 
 	# Move the character
 	velocity = move_and_slide(velocity, Vector2.UP)
+
+
+	# Keep the character within the screen bounds
+	var screen_size = get_viewport_rect().size
+	if position.x < 0:
+		position.x = 0
+	if position.x > screen_size.x:
+		position.x = screen_size.x
