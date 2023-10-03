109b66d9d064a79cf78f91bf07203e605346ebc6
GeloxGero <gelo12gero@gmail.com>
2023-10-03 13:32:32 +0800

    added jumping

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -15,8 +15,12 @@ var jump_force = 1
 
 func _physics_process(delta):
 	# Get input direction
+	#
+	# continuous click
 	var right = Input.is_action_pressed('ui_right')
 	var left = Input.is_action_pressed('ui_left')
+	
+	# per click
 	var down = Input.is_action_just_pressed('ui_down')
 	var up = Input.is_action_just_pressed('ui_up')
 	var on_floor = is_on_floor()
@@ -29,22 +33,27 @@ func _physics_process(delta):
 		if _state == States.CROUCH:
 			_state = States.ON_GROUND
 		elif _state == States.ON_GROUND:
+			animation.play("jump")
 			velocity.y = jump_speed * jump_force
 			_state = States.IN_AIR
 	elif(down):
 		if _state == States.ON_GROUND:
 			_state = States.CROUCH
 	elif(right):
+		if _state == States.ON_GROUND:
+			animation.play("run")
 		$Sprite.flip_h = false
 		direction.x += 1
-		animation.play("run")
 	elif(left):
+		if _state == States.ON_GROUND:
+			animation.play("run")
 		$Sprite.flip_h = true
 		direction.x -= 1
 		animation.play("run")
-		
 	else:
-		animation.play("idle")
+		if _state == States.ON_GROUND:
+			animation.play("idle")
+		
 	
 
 	# Normalize direction
