6b2fd891da8da004bfbafb4e1c36bfcb8e2f57f2
LRS0101 <leosalo64@gmail.com>
2023-10-17 15:18:55 +0800

    added transitions

--- Scripts/LevelA2.gd
+++ Scripts/LevelA2.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+var inc = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -11,22 +11,38 @@ func _ready():
 func _process(delta):
 	change_scene()
 
-
 func _on_edge_right_body_entered(body):
 	if body.has_method("player"):
+		inc = 1
 		Global.transition_scene = true
-	
+
 
 func _on_edge_right_body_exited(body):
 	if body.has_method("player"):
+		inc = 0
 		Global.transition_scene = false
+
+func _on_edge_left_body_entered(body):
+	if body.has_method("player"):
+		inc = -1
+		Global.transition_scene = true
+
+
+func _on_edge_left_body_exited(body):
+	if body.has_method("player"):
+		inc = 0
+		Global.transition_scene = false
+
 
 
 func change_scene():
 	if Global.transition_scene == true:
-		if Global.current_scene == "LevelA1":
-			get_tree().change_scene_to_file("res://Scenes/LevelA2.tscn")
+		if inc > 0:
+			get_tree().change_scene_to_file("res://Scenes/LevelA3.tscn")
 			Global.finish_changescenes(1)
-	
-	
+		elif inc < 0:
+			get_tree().change_scene_to_file("res://Scenes/LevelA1.tscn")
+			Global.finish_changescenes(-1)
+
+
 
--- Scripts/global.gd
+++ Scripts/global.gd
@@ -1,13 +1,16 @@
 extends Node
 
+var level_index = 0
 
-var current_scene = "LevelA1"
 var transition_scene = false
+
+var Levels = ["LevelA1", "LevelA2", "LevelA3", "LevelA4"]
+var current_scene = Levels[0]
 
 var player_LevelA1_right_posx = 0
 
-func finish_changescenes():
+func finish_changescenes(inc):
 	if transition_scene == true:
 		transition_scene = false
-		if current_scene == "LevelA1":
-			current_scene = "LevelA2"
+		level_index += inc
+		current_scene = Levels[level_index]
