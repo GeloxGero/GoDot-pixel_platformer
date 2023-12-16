19662e75ce65dff1fc5e6bc5235acc8b216d2b8b
GeloxGero <gelo12gero@gmail.com>
2023-12-16 14:32:42 +0800

    animated burning for switches

--- Scripts/gdEnvironment/switch.gd
+++ Scripts/gdEnvironment/switch.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 var has_player : bool = false
+var interacted : bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,12 +10,16 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if has_player and Input.is_action_just_pressed("Interact"):
-		$Sprite2D.show()
+		var sprite = $AnimatedSprite2D
+		sprite.play("default")
+		sprite.show()
+		interacted = true
 
 
 func _on_body_entered(body):
 	if body.has_method("mc"):
 		has_player = true
+		print(body)
 	
 		
 
