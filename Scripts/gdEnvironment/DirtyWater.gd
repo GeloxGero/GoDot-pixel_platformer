5f28fe9b5b8a3e3939d6cdf85b0a5dcf8ad2ece0
GeloxGero <gelo12gero@gmail.com>
2023-12-16 14:18:45 +0800

    water animation

---
+++ Scripts/gdEnvironment/DirtyWater.gd
@@ -0,0 +1,12 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	play('default')
+	rotation_degrees = 90
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
